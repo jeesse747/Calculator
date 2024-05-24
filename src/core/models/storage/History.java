d49902f1c9fb32e5e56ce9d50e571c0f91e752f3
Maria Romero <fmalvarado@uninorte.edu.co>
2024-05-23 22:57:17 -0500

    carpetas

---
+++ src/core/models/storage/History.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.models.storage;
+
+import core.models.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author edangulo
+ */
+public class History {
+    
+    private ArrayList<Operation> operations;
+
+    public History() {
+        this.operations = new ArrayList<>();
+    }
+    
+    public void addOperation(Operation operation) {
+        this.operations.add(operation);
+    }
+
+    public ArrayList<Operation> getOperations() {
+        return operations;
+    }
+    
+}
